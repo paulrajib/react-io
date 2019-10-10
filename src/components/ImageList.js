836c1a1f84aa494008945d427fc44d0075bfee3e
paulrajib <rajib.ece09@gmail.com>
2019-10-10 23:55:16 +0600

    communismic spacing for height :)

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,16 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
 	// console.log(props.images);
 
-	const images = props.images.map(({description, id, urls}) => {
-		return <img key={id} src={urls.regular} alt={description} />;
+	const images = props.images.map((image) => {
+		/*return <img key={id} src={urls.regular} alt={description} />;*/
+		return <ImageCard key={image.id} image={image} />;
 	});
 	
-	return <div>{images}</div>;
+	return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
